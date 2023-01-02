96fc5cf0979f8d07b9001b9f47e32f8f404d4559
AidenSky001 <87076299+AidenSky001@users.noreply.github.com>
2023-01-02 17:37:22 +0100

    erweiterung für das lesen und senden (unfert.)

--- client.c
+++ client.c
@@ -2,83 +2,95 @@
 #include <winsock2.h>
 #include <Windows.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main()
+#define BUFFER_SIZE 1024
+#define PORT 54321
+
+
+
+int main(int argc, char** argv)
 {
+    if (argc != 4){
+        printf("Benutze Datei: %s <filename> <destination IP> <port>\n", argv[0]);
+        return 1;
+    }
+
+    //Anlegen der Variablen die vom Nutzer als Eingangsargumente eingegeben werden
+    char* filename = argv[1];
+    char* dest_ip = argv[2];
+    int port = atoi(argv[3]);
+
     //Hilfsvariable
-    int antwort;
+
+    struct sockaddr_in server; 
 
     //Init von ws2_32.lib
     WSADATA wsaData;
 
     // Initialisiere Winsock
+    printf("Initalisiere Winsock ... \n");
+    WSAStartup(MAKEWORD(2,2), &wsaData);
 
-    antwort = WSAStartup(MAKEWORD(2,2), &wsaData);
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
     {
-        printf("Fehler bei WSAStartup\n");
+        printf("Fehler bei WSAStartup. Fehlercode: %d \n", WSAGetLastError()); //Gibt bei Fehlern auch den Fehlercode aus, lässt sich leichter debbugen
+        return 1;
     }
 
     // Socket Deskriptor
-    SOCKET slisten, client; //Hier wird der Socket slisten eingerichtet
+    SOCKET rnkssocket; //Hier wird der Socket rnkssocket eingerichtet
 
     // Socket einrichten
-    slisten = socket(AF_INET, SOCK_STREAM, 0);
-    if (slisten != INVALID_SOCKET)
+    printf("Erstelle den Socket ... \n");
+    rnkssocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (rnkssocket != INVALID_SOCKET)
     {
-        printf("Socket erfolgreich gestartet");
+        printf("Socket erfolgreich erstellt \n");
     }
     else
     {
-        printf("Socket einrichten fehlgeschlagen");
+        printf("Socket einrichten fehlgeschlagen \n");
     }
 
     //Verbindungsinformationen
     struct sockaddr_in info; // In C muss hier dieses Struct vorangesetzt werden, sonst geht das definieren nicht (keine Ahnung warum, in C++ ist das anders)
-    info.sin_addr.s_addr    = inet_addr("localhost"); //Die erreichbare Internet Adresse, hier könnte auch eine IP rein, ich will mich hier aber erstmal selbst anwählen
+    info.sin_addr.s_addr    = inet_addr(dest_ip); //Die erreichbare Internet Adresse, hier könnte auch eine IP rein, ich will mich hier aber erstmal selbst anwählen
     info.sin_family         = AF_INET; //Die Family ist wie oben die AF_INET
-    info.sin_port           = htons(54345); //Das ist irgendein Port mit einer festgelegten Größe
-    int infolen             = sizeof(info); //Gibt die Größe von der Info zurück
+    info.sin_port           = htons(PORT); //Das ist irgendein Port mit einer festgelegten Größe
 
-    antwort = bind(slisten, (struct sockaddr*)&info, infolen); //stuct sockaddr wird hier gecastet
-
-    if(antwort != SOCKET_ERROR)
-    {
-        printf("Der Bind war erfolgreich!");
-    }
-    else
-    {
-        printf("Bind fehlgeschlagen");
+    //Datei öffen
+    char buffer[BUFFER_SIZE];
+    printf("Datei wird eingelesen... \n");
+    FILE* fp = fopen(filename, "r");
+    if(fp == NULL){
+        printf("Fehler beim öffnen der Datei! \n");
+        return 1;
     }
 
-    //Warteschlange für einkommende Verbindungen
-    antwort = listen(slisten, SOMAXCONN); //Wie viele Verbindungen dürfen zugelassen werden, in diesem Beispiel sind es Max. also so viele wie gehen
-    if(antwort != SOCKET_ERROR)
-    {
-        printf("listen() war erfolgreich!");
-    }
-    else
-    {
-        printf("listen() fehlgeschlagen");
-    }
+    //Datei senden 
+    printf("Sende Datei ... \n");
+    // Empfange Nachricht
+struct sockaddr_in sender;
+int sender_len = sizeof(sender);
+char recv_buffer[BUFFER_SIZE];
+int recv_len = recvfrom(rnkssocket, recv_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&sender, &sender_len);
+if (recv_len > 0) {
+    recv_buffer[recv_len] = '\0';
+    printf("Nachricht empfangen: %s\n", recv_buffer);
+}
 
-    //Hier kommen die Client-Informationen
-    struct sockaddr_in clientinfo;
-    int clientinfolen = sizeof(clientinfo);
-
-    while(1) //Die Hauptschleife
-    {
-        client = accept(slisten, (struct sockaddr*)&clientinfo, &clientinfolen);
-        if (client != SOCKET_ERROR)
-        printf("client Akzeptiert");
-        printf(inet_ntoa(clientinfo.sin_addr));
-        printf(" : ");
-        //printf(ntohs(clientinfo.sin_port));
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL){
+        if (sendto(rnkssocket, buffer, strlen(buffer), 0, (struct sockaddr*)&server, sizeof(server)) < 0){
+            printf("Senden fehlgeschlagen! \n");
+        }
     }
 
-    closesocket(client);
-    closesocket(slisten);
+    fclose(fp);
+    closesocket(rnkssocket);
     WSACleanup();
 
+
+    printf("Goodbye. Stay healthy and wash your hands. 3301");
+    return 0;
 }
