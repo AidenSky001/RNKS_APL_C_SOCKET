4094d94346de0436c76f39d49be44b3cc395dfb8
AidenSky001 <87076299+AidenSky001@users.noreply.github.com>
2023-01-22 14:44:48 +0100

    Habe hier noch einiges neues drin.
    Speicherort für Serverausgabe Datei. Lenkbare Ports. Fehlermeldungen und Timer für den Client. 
    Fürs bestehen des Belegs sollte es hoffentlich reichen

--- client.c
+++ client.c
@@ -1,96 +1,101 @@
 #include <stdio.h>
+#include <string.h>
 #include <winsock2.h>
-#include <Windows.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <time.h>
+
 
 #define BUFFER_SIZE 1024
-#define PORT 54321
-
-
 
 int main(int argc, char** argv)
 {
     if (argc != 4){
-        printf("Benutze Datei: %s <filename> <destination IP> <port>\n", argv[0]);
+        printf("Zu wenige Argumente eingegeben: \n Benutze Datei: %s <filename> <destination IP> <port>\n", argv[0]);
         return 1;
     }
 
-    //Anlegen der Variablen die vom Nutzer als Eingangsargumente eingegeben werden
+    //Anlegen von Variablen
     char* filename = argv[1];
     char* dest_ip = argv[2];
-    int port = atoi(argv[3]);
+    int port = atoi(argv[3]); //Portnummer kann nur 16 Bit groß sein. Also maximal 65535 Ports // Alle Ports bis <1024 sind reserviert
 
-    //Hilfsvariable
-
-    struct sockaddr_in server; 
-
-    //Init von ws2_32.lib
-    WSADATA wsaData;
-
-    // Initialisiere Winsock
-    printf("Initalisiere Winsock ... \n");
-    WSAStartup(MAKEWORD(2,2), &wsaData);
-
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
-    {
-        printf("Fehler bei WSAStartup. Fehlercode: %d \n", WSAGetLastError()); //Gibt bei Fehlern auch den Fehlercode aus, lässt sich leichter debbugen
+    // Initialisieren von Winsock
+    WSADATA wsa;
+    u_long iMode = 1; //Socket wird auf nichtblockierend gesetzt
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Fehler bei WSAStartup\n");
         return 1;
     }
+    printf("WSA Initalisiert \n");
 
-    // Socket Deskriptor
-    SOCKET rnkssocket; //Hier wird der Socket rnkssocket eingerichtet
-
-    // Socket einrichten
-    printf("Erstelle den Socket ... \n");
-    rnkssocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (rnkssocket != INVALID_SOCKET)
-    {
-        printf("Socket erfolgreich erstellt \n");
+    // Erstellen des sockets
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == INVALID_SOCKET) {
+        printf("Fehler beim Erstellen des sockets \n");
+        WSACleanup();
+        return 1;
     }
-    else
-    {
-        printf("Socket einrichten fehlgeschlagen \n");
-    }
+    printf("Socket wurde erstellt \n");
 
-    //Verbindungsinformationen
-    struct sockaddr_in info; // In C muss hier dieses Struct vorangesetzt werden, sonst geht das definieren nicht (keine Ahnung warum, in C++ ist das anders)
-    info.sin_addr.s_addr    = inet_addr(dest_ip); //Die erreichbare Internet Adresse, hier könnte auch eine IP rein, ich will mich hier aber erstmal selbst anwählen
-    info.sin_family         = AF_INET; //Die Family ist wie oben die AF_INET
-    info.sin_port           = htons(PORT); //Das ist irgendein Port mit einer festgelegten Größe
+    // Adresse des Servers
+    SOCKADDR_IN serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(dest_ip);
+    serverAddr.sin_port = htons(port);
 
-    //Datei öffen
+     //Datei öffen
     char buffer[BUFFER_SIZE];
+    //Variablen zum Arbeiten
+    int sequenznummer =0;
+    char Zeilen[255];
+
     printf("Datei wird eingelesen... \n");
-    FILE* fp = fopen(filename, "r");
-    if(fp == NULL){
+    FILE* datei = fopen(filename, "r");
+    if(datei == NULL){
         printf("Fehler beim öffnen der Datei! \n");
         return 1;
     }
+    printf("Datei gefunden und eingelesen \n");
 
-    //Datei senden 
-    printf("Sende Datei ... \n");
-    // Empfange Nachricht
-struct sockaddr_in sender;
-int sender_len = sizeof(sender);
-char recv_buffer[BUFFER_SIZE];
-int recv_len = recvfrom(rnkssocket, recv_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&sender, &sender_len);
-if (recv_len > 0) {
-    recv_buffer[recv_len] = '\0';
-    printf("Nachricht empfangen: %s\n", recv_buffer);
-}
+    //Datei senden
+    while (fgets(buffer, BUFFER_SIZE, datei) != NULL){
+        sendto(sock, buffer, strlen(buffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+        printf("Zeilennummern: %d: %s", sequenznummer, buffer);
+        sequenznummer ++;
 
-    while (fgets(buffer, BUFFER_SIZE, fp) != NULL){
-        if (sendto(rnkssocket, buffer, strlen(buffer), 0, (struct sockaddr*)&server, sizeof(server)) < 0){
-            printf("Senden fehlgeschlagen! \n");
-        }
+        sprintf(Zeilen, "%d", sequenznummer);
+        sendto(sock, Zeilen, strlen(Zeilen), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+        ioctlsocket(sock, FIONBIO, &iMode);
+
+    //Stellen eines Timers
+    fd_set readfds;
+    struct timeval timeout;
+
+    FD_ZERO(&readfds);
+    FD_SET(sock, &readfds);
+
+    timeout.tv_sec = 5; 
+    timeout.tv_usec = 0;
+
+    //Auf Antwort des Servers wird gewartet
+    char recvBuf[1024];
+    int recvLen = recv(sock, recvBuf, sizeof(recvBuf), 0);
+    if(select(sock, &readfds, NULL, NULL, &timeout) <= 0)
+    {
+    printf("Der Timer ist abglaufen.");
+    printf("Die Zeile wird nochmal gesendet");
+    sendto(sock, buffer, strlen(buffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+    }
+    else
+    {
+    printf("Empfangene Nachricht vom Server: %s \n", recvBuf);
+    }
     }
 
-    fclose(fp);
-    closesocket(rnkssocket);
+    printf("Alle Daten gesendet \n");
+
+    // Schließen des sockets und Aufräumen von Winsock
+    closesocket(sock);
     WSACleanup();
 
-
-    printf("Goodbye. Stay healthy and wash your hands. 3301");
     return 0;
 }
--- server.c
+++ server.c
@@ -2,12 +2,30 @@
 #include <string.h>
 #include <winsock2.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    //Anlegen der Variablen
+    if (argc != 3){
+        printf("Zu wenige Argumente eingegeben: \n Datei Ausgabename: %s <filename> Port: <port>\n", argv[0]);
+        return 1;
+    }
+
+    //Anlegen von Variablen
+    char* filename = argv[1];
+    int port = atoi(argv[2]); //Portnummer kann nur 16 Bit groß sein. Also maximal 65535 Ports // Alle Ports bis <1024 sind reserviert
     int Nachrichtencounter =0;
     int Vergleichswert =1;
     int Sequenzbild =1;
+
+    if (port < 1024)
+    {
+        printf("Bitte einen Port groeßer 1024 waehlen. Ports unter 1025 sind reserviert");
+        return 1;
+    }
+    if (port > 65535)
+    {
+        printf("Bitte eine kleinere Porstnummer verwenden. Nur Portnummern bis 65535 sind zulaessig.");
+        return 1;
+    }
 
     // Initialisieren von Winsock
     WSADATA wsa;
@@ -30,7 +48,7 @@ int main()
     SOCKADDR_IN addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(5000);
+    addr.sin_port = htons(port);
     if (bind(sock, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) {
         printf("Fehler beim Binden des sockets\n");
         closesocket(sock);
@@ -43,12 +61,19 @@ int main()
     char buffer[1024];
     SOCKADDR_IN clientAddr;
     int clientAddrLen = sizeof(clientAddr);
+    //Schreiben der Nachricht in eine serperate Datei
+    //"a" ist der Append Modus wo immer weitergeschrieben wird statt zu überschreiben
+    FILE *file = fopen(filename, "a"); 
+
     while (1) {
         int recvLen = recvfrom(sock, buffer, sizeof(buffer), 0, (SOCKADDR*)&clientAddr, &clientAddrLen);
         if (recvLen > 0) {
             buffer[recvLen] = '\0';
             printf("Empfangen von IP %s: Port %d: %s \n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buffer);
             Nachrichtencounter++; //Sorgt dafür das die Nachrichten Nacheinander eine Nummer bekommen, welche mit der Sequenznummer abgelichen werden können
+
+            fwrite(buffer, sizeof(char), recvLen, file); //Schreiben der Daten in eine Datei
+
             if (Nachrichtencounter %2 == 0)
             {
                 sscanf(buffer, "%i", &Sequenzbild);
@@ -57,8 +82,11 @@ int main()
                 if(Vergleichswert == Sequenzbild){
                     printf("Die Sequenzen sind gleich. \n");
 
+                    //Für Testzwecke kann folgende Methode verwendet werden:
+                    //Sleep(6000); 
+
                     //Die Antwort des Servers an den Client
-                    char sendeAntwort[] = "Die Sequenznummer stimmt.";
+                    char sendeAntwort[] = "Die Sequenznummer stimmt. \n";
                     int sendLen = strlen(sendeAntwort) +1;
                     sendto(sock, sendeAntwort, sendLen, 0, (SOCKADDR*)&clientAddr, clientAddrLen);
                     printf("Antwort an Client gesendet");
@@ -75,7 +103,8 @@ int main()
         }
     }
 
-    // Schließen des sockets und Aufräumen von Winsock
+    // Schließen des Filewriters, Schließen des Sockets und Aufräumen von Winsock
+    fclose(file);
     closesocket(sock);
     WSACleanup();
 
